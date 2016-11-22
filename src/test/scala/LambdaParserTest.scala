13338b556fb412ffc5b4a1c4c624b1f66762493f
Ahmed Metwally <ametwa2@uic.edu>
2016-11-21 20:58:01 -0600

    update

--- src/test/scala/LambdaParserTest.scala
+++ src/test/scala/LambdaParserTest.scala
@@ -1,23 +1,12 @@
-//import org.junit.Test
 import org.junit.Test
 
 /**
-  * Created by hady on 11/21/16.
+  * Created by AhmedMetwally on 11/21/16.
   */
 class LambdaParserTest extends org.scalatest.FunSuite {
 
   @Test
   def testParser {
-//    val projectPath : String = "datasets/square"
-//    val list_of_version_subfolders = actor.createUDBFiles(projectPath)
-
-
-//    Assert("4")
-//    assert(Files.exists(Paths.get("okio.udb")))
-//    assert(Files.exists(Paths.get("okio_v2.udb")))
-
-
-
     var lmbdaExpr : String = "(λx.x) t"
     var expected_output = "A(L(V(x),V(x)),V(t))"
     val prs = new LambdaParser()
@@ -34,5 +23,4 @@ class LambdaParserTest extends org.scalatest.FunSuite {
     }
       println("Passed Test Parser...")
   }
-
 }
---
+++ src/test/scala/UnitTest.scala
@@ -0,0 +1,11 @@
+import java.nio.file.{Files, Paths}
+
+import org.junit.{Assert, Test}
+
+class UnitTest extends org.scalatest.FunSuite {
+  @Test
+  def testUDB {
+    assert(2==2)
+    println("Passed...")
+  }
+}
